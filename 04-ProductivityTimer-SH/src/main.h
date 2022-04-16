85143a2282289b77b1a599e8f7f9509f34d071e8
xpress_embedo <laboratory.embedded@gmail.com>
2022-04-16 23:24:45 +0200

    04-ProductivityTimer-StateHandler Approach * Tested and Working fine. * Here function pointer is used for main State Engine

---
+++ 04-ProductivityTimer-SH/src/main.h
@@ -0,0 +1,92 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include "lcd.h"
+
+#define PIN_BUTTON1             2u
+#define PIN_BUTTON2             3u
+#define PIN_BUTTON3             4u
+#define PIN_BUZZER              12u
+
+/* LCD Pin Connections */
+#define PIN_LCD_RS              5u
+#define PIN_LCD_RW              6u
+#define PIN_LCD_EN              7u
+#define PIN_LCD_D4              8u
+#define PIN_LCD_D5              9u
+#define PIN_LCD_D6              10u
+#define PIN_LCD_D7              11u
+
+#define BTN_PAD_VALUE_INC_TIME  4u
+#define BTN_PAD_VALUE_DEC_TIME  2u
+#define BTN_PAD_VALUE_ABRT      6u
+#define BTN_PAD_VALUE_SP        1u
+
+typedef enum
+{
+  NOT_PRESSED,
+  BOUNCE,
+  PRESSED
+} button_state_t;
+
+/* Signals of the Application */
+typedef enum
+{
+  INC_TIME = 0,
+  DEC_TIME,
+  TIME_TICK,
+  START_PAUSE,
+  ABRT,
+  /* Internal Activity Signals */
+  ENTRY,
+  EXIT
+} protimer_signal_t;
+
+/* Generic (Super) Event Structure */
+typedef struct 
+{
+  uint8_t sig;
+} event_t;
+
+/* For user generated events */
+typedef struct 
+{
+  event_t super;
+} protimer_user_event_t;
+
+/* For Tick Events */
+typedef struct 
+{
+  event_t super;
+  uint8_t ss;
+} protimer_tick_event_t;
+
+typedef enum
+{
+  EVENT_HANDLED = 0,
+  EVENT_IGNORED,
+  EVENT_TRANSITION
+} event_status_t;
+
+/* forward declaration for protimer_t structure because it is declared below but
+we have to use it in typedef alias for function pointer, if there is something else
+which can cause problem, then we can use this forward declaration method */
+struct _protimer_state_t;
+typedef event_status_t (*protimer_state_t)( struct _protimer_state_t  *const , event_t const *const);
+
+/* Main Application Structure */
+typedef struct _protimer_state_t
+{
+  uint32_t          curr_time;
+  uint32_t          elapsed_time;
+  uint32_t          pro_time;
+  protimer_state_t  active_state;        
+} protimer_t;
+
+
+/* Public Function Prototpes */
+void protimer_init( protimer_t *mobj );
+
+#endif 
---
+++ 04-ProductivityTimer-SH/src/protimer_state_machine.cpp
@@ -0,0 +1,274 @@
+#include "main.h"
+#include "lcd.h"
+
+/* Function Pointer Callbacks short form using Macros */
+#define IDLE          &protimer_state_handler_IDLE
+#define TIME_SET      &protimer_state_handler_TIME_SET
+#define COUNTDOWN     &protimer_state_handler_COUNTDOWN
+#define PAUSE         &protimer_state_handler_PAUSE
+#define STAT          &protimer_state_handler_STAT
+
+/* Private Function Prototypes */
+static void do_beep( void );
+static void display_clear( void );
+static void display_message( String s,uint8_t c , uint8_t r );
+static void display_time( uint32_t time );
+
+static event_status_t protimer_state_handler_IDLE( protimer_t  *const mobj, event_t const *const e );
+static event_status_t protimer_state_handler_TIME_SET( protimer_t  *const mobj, event_t const *const e );
+static event_status_t protimer_state_handler_COUNTDOWN( protimer_t  *const mobj, event_t const *const e );
+static event_status_t protimer_state_handler_PAUSE( protimer_t  *const mobj, event_t const *const e );
+static event_status_t protimer_state_handler_STAT( protimer_t  *const mobj, event_t const *const e );
+
+/* Public Function Definitions */
+void protimer_init( protimer_t *mobj )
+{
+  event_t entry_action;
+  entry_action.sig = ENTRY;
+  mobj->active_state = IDLE;
+  mobj->pro_time = 0u;
+  /* call the function */
+  (*mobj->active_state)( mobj, &entry_action ); 
+}
+
+/* Private Function Definitions */
+static event_status_t protimer_state_handler_IDLE( protimer_t *const mobj, event_t const *const e )
+{
+  switch( e->sig )
+  {
+    case ENTRY:
+    {
+      mobj->curr_time = 0;
+      mobj->elapsed_time = 0;
+      display_time(0);
+      display_message("Set", 0, 0);
+      display_message("time", 0, 1);
+      return EVENT_HANDLED;
+    }
+    case EXIT:
+    {
+      display_clear();
+      return EVENT_HANDLED;
+    }
+    case INC_TIME:
+    {
+      mobj->curr_time += 60;
+      mobj->active_state = TIME_SET;
+      return EVENT_TRANSITION;
+    }
+    case START_PAUSE:
+    {
+      mobj->active_state = STAT;
+      return EVENT_TRANSITION;
+    }
+    case TIME_TICK:
+    {
+      /* turn on the buzzer every 500ms */
+      if( ((protimer_tick_event_t *)(e))->ss == 5)
+      {
+        do_beep();
+        return EVENT_HANDLED;
+      }
+      return EVENT_IGNORED;
+    }
+  } /* End of Switch */
+
+  return EVENT_IGNORED;
+}
+
+static event_status_t protimer_state_handler_TIME_SET(protimer_t *const mobj, event_t const *const e)
+{
+  switch( e->sig )
+  {
+    case ENTRY:
+    {
+      display_time( mobj->curr_time );
+      return EVENT_HANDLED;
+    }
+    case EXIT:
+    {
+      display_clear();
+      return EVENT_HANDLED;
+    }
+    case INC_TIME:
+    {
+      mobj->curr_time += 60;
+      display_time( mobj->curr_time );
+      return EVENT_HANDLED;
+    }
+    case DEC_TIME:
+    {
+      if( mobj->curr_time >=60 )
+      {
+        mobj->curr_time -= 60;
+        display_time( mobj->curr_time );
+        return EVENT_HANDLED;
+      }
+      return EVENT_IGNORED;
+    }
+    case ABRT:
+    {
+      mobj->active_state = IDLE;
+      return EVENT_TRANSITION;
+    }
+    case START_PAUSE:
+    {
+      if( mobj->curr_time >= 60 )
+      {
+        mobj->active_state = COUNTDOWN;
+        return EVENT_TRANSITION;
+      }
+      return EVENT_IGNORED;
+    }
+  }
+  return EVENT_IGNORED;
+}
+
+static event_status_t protimer_state_handler_COUNTDOWN( protimer_t *const mobj, event_t const *const e )
+{
+  switch(e->sig)
+  {
+    case EXIT:
+    {
+      mobj->pro_time += mobj->elapsed_time;
+      mobj->elapsed_time = 0;
+      return EVENT_HANDLED;
+    }
+
+    case TIME_TICK:
+    {
+      if( ((protimer_tick_event_t*)(e))->ss == 10 )
+      {
+        --mobj->curr_time;
+        ++mobj->elapsed_time;
+        display_time(mobj->curr_time);
+        if( !mobj->curr_time )
+        {
+          mobj->active_state = IDLE;
+          return EVENT_TRANSITION;
+        }
+        return EVENT_HANDLED;
+      }
+      return EVENT_IGNORED;
+    }
+    case START_PAUSE:
+    {
+      mobj->active_state = PAUSE;
+      return EVENT_TRANSITION;
+    }
+    case ABRT:
+    {
+      mobj->active_state = IDLE;
+      return EVENT_TRANSITION;
+    }
+  }
+  return EVENT_IGNORED;
+}
+
+static event_status_t protimer_state_handler_PAUSE( protimer_t *const mobj, event_t const *const e )
+{
+  switch(e->sig)
+  {
+    case ENTRY:
+    {
+      display_message("Paused", 5, 1);
+      return EVENT_HANDLED;
+    }
+    case EXIT:
+    {
+      display_clear();
+      return EVENT_HANDLED;
+    }
+    case INC_TIME:
+    {
+      mobj->curr_time += 60;
+      mobj->active_state = TIME_SET;
+      return EVENT_TRANSITION;
+    }
+    case DEC_TIME:
+    {
+      if(mobj->curr_time >= 60)
+      {
+        mobj->curr_time -= 60;
+        mobj->active_state = TIME_SET;
+        return EVENT_TRANSITION;
+      }
+      return EVENT_IGNORED;
+    }
+    case START_PAUSE:
+    {
+      mobj->active_state = COUNTDOWN;
+      return EVENT_TRANSITION;
+    }
+    case ABRT:
+    {
+      mobj->active_state = IDLE;
+      return EVENT_TRANSITION;
+    }
+  }
+  return EVENT_IGNORED;
+}
+
+static event_status_t protimer_state_handler_STAT( protimer_t *const mobj, event_t const *const e )
+{
+  static uint8_t tick_count;
+
+  switch( e->sig )
+  {
+    case ENTRY:
+    {
+      display_time( mobj->pro_time );
+      display_message("Productive time", 1, 1);
+      return EVENT_HANDLED;
+    }
+    case EXIT:
+    {
+      display_clear();
+      return EVENT_HANDLED;
+    }
+    case TIME_TICK:
+    {
+      tick_count++;
+      if( tick_count == 30 )
+      {
+        tick_count = 0;
+        mobj->active_state = IDLE;
+        return EVENT_TRANSITION;
+      }
+      return EVENT_IGNORED;
+    }
+  }
+  return EVENT_IGNORED;
+}
+
+/* Some Helper Functions */
+static void display_time( uint32_t time )
+{
+  char buf[7];
+  String time_msg;
+  
+  uint16_t minutes = time / 60;
+  uint8_t seconds = time % 60;
+  sprintf(buf,"%03d:%02d", minutes, seconds );
+  
+  time_msg = (String)buf;
+  lcd_set_cursor( 5, 0 );
+  lcd_print_string( time_msg );
+}
+
+static void display_message( String s, uint8_t c , uint8_t r )
+{
+  lcd_set_cursor( c, r);
+  lcd_print_string( s );
+}
+
+static void display_clear(void)
+{
+  lcd_clear();
+}
+
+static void do_beep(void)
+{
+  /* buzzer frequency is 2Khz, and run the buzzer for 200 ms */
+  tone(PIN_BUZZER, 2000, 200);
+}
